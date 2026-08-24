b5870e7cfde721549484e4732a9e5466144b2a17
I-Cant-Name-Myself <ktadmn@icloud.com>
2026-08-24 16:22:07 +0100

    Add C# API project

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpAIProject.Controllers;
+
+[ApiController]
+[Route("/testcase")]// ruote for local host API to go to
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            status = "healthy"
+        });
+    }
+}
